b9c8462eddf367cbc59646f0362d5b7f9b6dfda9
sharath-c99 <sharath.nlobng@gmail.com>
2021-05-28 20:49:41 +0530

    Web dev commit

---
+++ DOM/index.js
@@ -0,0 +1,7 @@
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+// var randomNum1=Math.floor(Math.random()*6)+1;
+
+// document.querySelector(".img1").setAttribute("src", "images/dice" + randomNum1 + ".png");
